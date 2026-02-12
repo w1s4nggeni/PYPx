0c2cbbeae749021066a6799ea2d1102f59cce111
w1s4nggeni <142919347+w1s4nggeni@users.noreply.github.com>
2026-02-12 22:45:36 +0700

    Change base path in Vite config to root

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: './',
+  base: '/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, '.'),
-    }
-  }
 });
