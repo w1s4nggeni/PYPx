de1173beecca2008e8d0b3805389f94861af3a05
w1s4nggeni <142919347+w1s4nggeni@users.noreply.github.com>
2026-02-12 21:45:20 +0700

    Update Vite config to set base path and plugins

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,11 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
+export default defineConfig({
+  base: './',
+  plugins: [react()]
+})
+
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
